05251679fe29dd0b45f833aa2113e10eb76355bc
e1ehpark <ehpark@enterprise1.co.kr>
2024-02-23 13:32:57 +0900

    수정

--- recursion/recursion.go
+++ recursion/recursion.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func fact(n int) int {
 	if n == 0 {
 		return 1
@@ -8,5 +10,17 @@ func fact(n int) int {
 }
 
 func main() {
-	fmt
+	fmt.Println(fact(7))
+
+	var fib func(n int) int
+
+	fib = func(n int) int {
+		if n < 2 {
+			return n
+		}
+
+		return fib(n-1) + fib(n-2)
+	}
+
+	fmt.Println(fib(7))
 }
