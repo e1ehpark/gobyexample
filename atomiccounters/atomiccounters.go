5c1b0a87514e9144302848c5248fc751c78f7233
e1ehpark <ehpark@enterprise1.co.kr>
2024-03-29 10:35:37 +0900

    update

---
+++ atomiccounters/atomiccounters.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"sync/atomic"
+)
+
+func main() {
+	var ops uint64
+
+	var wg sync.WaitGroup
+
+	for i := 0; i < 50; i++ {
+		wg.Add(1)
+
+		go func() {
+			for i := 0; i < 1000; i++ {
+				ops.Add(1)
+			}
+
+			wg.Done()
+		}()
+	}
+
+	wg.Wait()
+
+	fmt.Println("ops:", ops.Load())
+}
