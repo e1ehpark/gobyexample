c81caa2a7a59b8167c8cdb019aca9320f07c8ae6
e1ehpark <ehpark@enterprise1.co.kr>
2024-04-17 14:38:26 +0900

    update

---
+++ context/context.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+func hello(w http.ResponseWriter, req *http.Request) {
+	log.SetFlags(log.LstdFlags | log.Lmicroseconds)
+	ctx := req.Context()
+	log.Println("server: hello handler started")
+	defer log.Println("server: hello handler ended")
+
+	select {
+	case <-time.After(10 * time.Second):
+		fmt.Fprintf(w, "hello\n")
+		log.SetOutput(w)
+	case <-ctx.Done():
+
+		err := ctx.Err()
+		log.Println("server:", err)
+		internalError := http.StatusInternalServerError
+		http.Error(w, err.Error(), internalError)
+
+	}
+}
+
+func main() {
+	http.HandleFunc("/hello", hello)
+	http.ListenAndServe(":8090", nil)
+}
