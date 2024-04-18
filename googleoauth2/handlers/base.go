c3c398bd5f021d10359f5fbd3672a9935ca2c6df
e1ehpark <ehpark@enterprise1.co.kr>
2024-04-18 17:02:01 +0900

    update

--- googleoauth2/handlers/base.go
+++ googleoauth2/handlers/base.go
@@ -8,8 +8,8 @@ func New() http.Handler {
 	mux := http.NewServeMux()
 	mux.Handle("/", http.FileServer(http.Dir("templates/")))
 
-	mux.handleFunc("/auth/google/login", oauthGoogleLogin)
-	mux.handleFunc("/auth/google/callback", oauthGoogleCallback)
+	mux.HandleFunc("/auth/google/login", oauthGoogleLogin)
+	mux.HandleFunc("/auth/google/callback", oauthGoogleCallback)
 
 	return mux
 }
