3686322eea4fc870735127222a87c49acf0e4b9f
e1ehpark <ehpark@enterprise1.co.kr>
2024-04-18 17:20:26 +0900

    update

--- googleoauth2/handlers/base.go
+++ googleoauth2/handlers/base.go
@@ -1,12 +1,16 @@
 package handlers
 
 import (
+	"log"
 	"net/http"
 )
 
 func New() http.Handler {
 	mux := http.NewServeMux()
+	log.SetFlags(log.LstdFlags | log.Lmicroseconds)
+	log.Println("Handler start")
 	mux.Handle("/", http.FileServer(http.Dir(".\\googleoauth2\templates")))
+	log.Println("/ started")
 
 	mux.HandleFunc("/auth/google/login", oauthGoogleLogin)
 	mux.HandleFunc("/auth/google/callback", oauthGoogleCallback)
